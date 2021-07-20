0e9e49b7845c0f96c63ea369627083b4f4e3000e
laneskell <laneskell@gmail.com>
2021-07-20 14:40:44 -0300

    implementação textField delegate

--- faceIDLogin/faceIDLogin/WelcomeViewController.swift
+++ faceIDLogin/faceIDLogin/WelcomeViewController.swift
@@ -7,21 +7,83 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
-
+class WelcomeViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textFieldTypedText: UITextField!
+    @IBOutlet weak var labelTypedTextResponse: UILabel!
+    
+    var parentVC: LoginViewController?
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        textFieldTypedText.delegate = self
+        
+    }
 
-        // Do any additional setup after loading the view.
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool  {
+            let newString = textFieldTypedText.text
+            let countString = Int(newString?.count ?? 0)
+                if countString >= 0 {
+                    labelTypedTextResponse.text = ("Você digitou \(countString) caracteres")
+                } else {
+                    labelTypedTextResponse.text = ("Você digitou 0 caracteres")
+                }
+        return true
+//      EXECUTA A CADA MUDANÇA FEITA NO CAMPO
     }
     
     @IBAction func btnLogout(_ sender: Any) {
-        navigationController?.popViewController(animated: true)
-        dismiss(animated: true, completion: nil)
-        UserDefaults.standard.removeObject(forKey: "email")
-        UserDefaults.standard.removeObject(forKey: "pwd")
+        self.parentVC?.logoutUser()
+        dismiss(animated: true)
     }
+    
+    
+    
+
+//      TESTE
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+//      EXECUTA AO CLICAR NO CAMPO
+        print("O usuário clicou no campo ")
+        return true
     }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+//      PRONTO PARA EDIÇÃO
+        print("o usuário pode iniciar a edição")
+//      Parece igual ao clicar no campo
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+//      EXECUTA AO LIMPAR O CAMPO
+        print("O usuário limpou o campo")
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool  {
+//      EXECUTA AO ENVIAR - ENTER/RETURN
+        print("Click em Enter/Return")
+        return true
+    }
+  
+    //ESTAVA PEGANDO A ALTERAÇÃO E CHAMANDO NA FUNCAO DO BOTÃO TESTE
+    //    func textFieldDidChange(textField: UITextField) {
+    //        let newString = textFieldTypedText.text
+    //        let countString = Int(newString?.count ?? 0)
+    //            if countString >= 0 {
+    //                labelTypedTextResponse.text = ("Você digitou \(countString) caracteres")
+    //            } else {
+    //                labelTypedTextResponse.text = ("Você digitou 0 caracteres")
+    //            }
+    //    }
+    //
+    //    @IBAction func botaoTeste(_ sender: Any) {
+    //        self.textFieldDidChange(textField: textFieldTypedText)
+    //    }
+
+        
+    
+}
     
 
 
